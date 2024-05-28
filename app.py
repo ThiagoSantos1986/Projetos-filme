e822d566982a5f24e47c6ee5ce60af5fcf85ae59
Thiago Cosme <tiagomutle@hotmail.com>
2024-05-28 15:21:53 -0300

    add new template filmes and new route filmes

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 from flask import Flask, render_template, request
 import requests, json
 
-headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Y2JlNTdlOTY2MjBlODAyYWVhYTAwMGNlZTdmZTAwZSIsInN1YiI6IjY2NGM0MzljYzk4ZTAxZDNjZDU0YWI5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UMcaacBy8xiR1EdAsuN0IznBpwxSmBSl9FauEPy1ipI"
-}
-
 app = Flask(__name__, template_folder="templates")
 frutas = []
 registros = [] 
@@ -32,13 +27,18 @@ def sobre():
 @app.route("/filmes")
 def filmes():
     url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4cbe57e96620e802aeaa000cee7fe00e"
-
-
+    
+    # url = "https://api.themoviedb.org/3/movie/upcoming"
+    # headers = {
+    #     "accept": "application/json",
+    #     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Y2JlNTdlOTY2MjBlODAyYWVhYTAwMGNlZTdmZTAwZSIsInN1YiI6IjY2NGM0MzljYzk4ZTAxZDNjZDU0YWI5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UMcaacBy8xiR1EdAsuN0IznBpwxSmBSl9FauEPy1ipI"
+    # }
+    
     response = requests.get(url)
 
     json_data = json.loads(response.text)
 
-    print(json_data['results'])
+    return  render_template("filmes.html",filmes=json_data['results'])
 
 
 
