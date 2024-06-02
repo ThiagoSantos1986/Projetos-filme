983c96ccd216229f39b701fe0d33b23d2021ae97
Thiago Cosme <tiagomutle@hotmail.com>
2024-06-02 05:02:15 -0300

    correção de bug pip

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 import requests, json
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder="templates")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.cursos"
+db = SQLAlchemy(app)
+
 frutas = []
 registros = [] 
 
@@ -54,6 +58,8 @@ def filmes(propriedade):
 
 
 if __name__ == "__main__":
+    # with app.app_context():
+    #     db.create_all()
 
     app.run(debug=True)
 
